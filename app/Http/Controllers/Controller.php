433f051bc48aa8dc1fab0b0697679a2548a47952
hebaArakha <hebatullah.ammar.rakha@gmail.com>
2024-06-27 13:13:28 +0300

    enhance home controller performance

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -20,17 +20,21 @@ class Controller extends BaseController
 
         $cacheDuration = 5 * 60; //cache for 5 minutes
         $recents = Cache::remember('recents', $cacheDuration, function () {
-            return Article::select('articles.id', 'articles.title', 'articles.created_at', 'articles.thumbnail_url', 'users.name as author_name')->join('users', 'articles.author_id', '=', 'users.id')->orderBy('articles.created_at', 'desc')->take(4)->get();
+            return Article::select('articles.id', 'articles.title', 'articles.created_at', 'articles.thumbnail_url', 'users.name as author_name')
+                ->join('users', 'articles.author_id', '=', 'users.id')
+                ->orderBy('articles.created_at', 'desc')
+                ->take(4)
+                ->get();
         });
         $populars = Cache::remember('populars', $cacheDuration, function () {
             return Article::select('articles.id', 'articles.title', 'articles.created_at', 'articles.thumbnail_url', 'users.name as author_name')
                 ->join('users', 'articles.author_id', '=', 'users.id')
-                ->withCount('comment') // Adjust the relationship name to match your actual relationship
+                ->withCount('comment')
                 ->orderBy('comment_count', 'desc')
                 ->take(4)
                 ->get();
         });
-        
+
 
         return view('home.index', [
             'recents' => $recents,
